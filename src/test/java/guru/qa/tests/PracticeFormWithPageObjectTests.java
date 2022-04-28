4fdedf310e852341941da775a418ba9f4c4cc1d3
LeybushAndrey <leiban@mail.ru>
2022-04-28 23:04:06 +0300

    lesson 5 28.04.22

---
+++ src/test/java/guru/qa/tests/PracticeFormWithPageObjectTests.java
@@ -0,0 +1,9 @@
+package guru.qa.tests;
+
+import pages.RegistrationPage;
+
+public class PracticeFormWithPageObjectTests extends TestBase{
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+}
---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+
+    private final String FORM_TITLE = "Student Registration Form";
+    private SelenideElement
+            formTitle = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName");
+    public CalendarComponent calendar = new CalendarComponent();
+
+    public void openPage() {
+        open("/automation-practice-form");
+        formTitle.shouldHave(text(FORM_TITLE));
+    }
+
+    public RegistrationPage typeFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public void typeLastName(String value) {
+        lastNameInput.setValue(value);
+    }
+
+    public void checkResultsValue(String key, String value) {
+        $x("//td[text()='" + key + "']").parent()
+                .shouldHave(text(value));
+    }
+}
