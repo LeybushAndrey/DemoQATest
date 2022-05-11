737ca7388d39b90cfabd493a3bf0d3ec4d411df5
LeybushAndrey <leiban@mail.ru>
2022-05-12 01:37:27 +0300

    lesson 5 06.05.22

--- src/test/java/guru/qa/tests/PracticeFormTest.java
+++ src/test/java/guru/qa/tests/PracticeFormTest.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeFormTest extends TestBase{
+public class PracticeFormTest{
 
 
     @Test
--- src/test/java/guru/qa/tests/PracticeFormWithPageObjectTests.java
+++ src/test/java/guru/qa/tests/PracticeFormWithPageObjectTests.java
@@ -1,9 +1,21 @@
 package guru.qa.tests;
 
+import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
+
+import static guru.qa.tests.TestData.firstName;
+import static guru.qa.tests.TestData.lastName;
 
 public class PracticeFormWithPageObjectTests extends TestBase{
 
     RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void fillFormTest() {
+        registrationPage.openPage();
+
+        registrationPage.typeFirstName(firstName)
+                        .typeLastName(lastName);
+    }
 
 }
